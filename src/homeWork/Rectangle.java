e713b3ffbe8b1a295f348d9857bfe49d26c80852
Александр <britishop@gmail.com>
2016-02-25 10:47:45 +0200

    Study

--- src/homeWork/Rectangle.java
+++ src/homeWork/Rectangle.java
@@ -6,17 +6,29 @@ package homeWork;
 public class Rectangle {
 
     int area;
-    int diagonal;
-    int sideAB;
-    int sideCD;
     int perimeter;
-    int x, y;
 
-    public int sidesCalculation() {
+    int array[] = new int[area];
 
-        x = perimeter / 2 - y;//11-y
+    public void init() {
+
+        for (int i = 0; i < area; i++) {
+            array[i] = i + 1;
+        }
+    }
 
 
+    public void sidesCalculation() {
+
+        for (int i = 0; i < area; i++) {
+            for (int j = 0; j < area; j++) {
+                if ((array[i] + array[j] == perimeter / 2) && (array[i] * array[j] == area)) {
+                    System.out.println("The sides of the rectangle are " + array[i] + " and " + array[j]);
+                    i = area;
+                    break;
+                }
+            }
+        }
     }
 
 }
