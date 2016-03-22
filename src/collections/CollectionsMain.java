023ad2e0561f37ea9446ee2c1a37ddcbc43753f8
ASurzhenko <britishop@gmail.com>
2016-03-22 21:06:12 +0200

    TDD, LinkedList, List, Set

--- src/collections/CollectionsMain.java
+++ src/collections/CollectionsMain.java
@@ -2,7 +2,7 @@ package collections;
 
 import OOP.Cat;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by User on 18.03.2016.
@@ -16,8 +16,40 @@ public class CollectionsMain {
 
         System.out.println(list);
 
-        Cat tom = (Cat)list.get(1);
+        Cat tom = (Cat) list.get(2);
 
-        System.out.println(tom);
+        LinkedList<Cat> linkedList = new LinkedList<>();
+        linkedList.add(new Cat("tom", "grey", 2015));
+        linkedList.add(new Cat("tom", "grey", 2015));
+        linkedList.add(new Cat("tom", "grey", 2015));
+
+        System.out.println(linkedList.get(1));
+
+        printList(list);
+        printList(linkedList);
+
+        Collection<Cat> cats = linkedList;
+
+        TreeSet<Cat> sortedCats = new TreeSet<>();
+
+        sortedCats.add(new Cat("tom", "grey", 2015));
+        sortedCats.add(new Cat("tom", "grey", 2011));
+        sortedCats.add(new Cat("tom", "grey", 2013));
+        sortedCats.add(new Cat("Murzic", "grey", 2013));
+
+        System.out.println("Set: ");
+        printForEach(sortedCats);
+    }
+
+    public static void printForEach(Collection<Cat> collection) {
+        for (Cat cat : collection) {
+            System.out.println(cat);
+        }
+    }
+
+    public static void printList(List<Cat> cats) {
+        for(int i = 0; i < cats.size(); i++) {
+            System.out.println(cats.get(i));
+        }
     }
 }
