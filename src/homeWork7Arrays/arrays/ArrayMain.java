ba162e1948dd629248b0db83c38db5f1f9dce3fc
ASurzhenko <britishop@gmail.com>
2016-04-12 21:09:01 +0300

    pattern decorator

--- src/homeWork7Arrays/arrays/ArrayMain.java
+++ src/homeWork7Arrays/arrays/ArrayMain.java
@@ -5,56 +5,16 @@ package homeWork7Arrays.arrays;
  */
 public class ArrayMain {
     public static void main(String[] args) {
-        /*Scanner scanner = new Scanner(System.in);
-        ImprovedArray firstArray = new ImprovedArray();
-        ImprovedArray secondArray = new ImprovedArray();
-
-        System.out.println("Fill in the first array. Write \"end\" to finish:");
-        fillTheArray(scanner, firstArray);
-
-        System.out.println("Enter the index of the element of the first array:");
-        printElement(scanner, firstArray);
-
-        System.out.println("Fill in the second array. Write \"end\" to finish:");
-        fillTheArray(scanner, secondArray);
-
-        System.out.println("Enter the index of the element of the second array:");
-        printElement(scanner, secondArray);
-
-        if (firstArray.equals(secondArray)) {
-            System.out.println("The first array equals the second one");
-        } else {
-            System.out.println("The first array don't equal the second one");
-        }
-
-        firstArray.remove("3");
-        //firstArray.remove(2);
-
-        System.out.println(firstArray.toString());
-        System.out.println(secondArray.toString());
-    }
-
-    private static void printElement(Scanner scanner, ImprovedArray improvedArray) {
-        int index = scanner.nextInt();
-        System.out.println("The element of index " + index + " is " + improvedArray.get(index));
-    }
-
-    private static void fillTheArray(Scanner scanner, ImprovedArray improvedArray) {
-        String value;
-        do {
-            value = scanner.next();
-            if (value.equals("end")) {
-                break;
-            }
-            improvedArray.add(value);
-        } while (!value.equals("end"));*/
 
         ImprovedArray improvedArray = new ImprovedArray();
-        ImprovedArray improvedArray2 = new ImprovedArray();
 
         improvedArray.add("cat");
-        improvedArray2.add("cat");
+        improvedArray.add("cat1");
+        improvedArray.add("cat2");
 
-        System.out.println(improvedArray.equals(improvedArray2));
+       /* for (Object o : improvedArray;)
+        {
+
+        }*/
     }
 }
