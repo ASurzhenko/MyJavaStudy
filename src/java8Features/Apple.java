3ddd2123b2cdbc408379b49cf9d2ed30b5c8b718
Александр <britishop@gmail.com>
2016-06-15 16:27:25 +0300

    streams

--- src/java8Features/Apple.java
+++ src/java8Features/Apple.java
@@ -8,6 +8,10 @@ public class Apple {
     private String color;
     private int price;
 
+    public Apple(){
+
+    }
+
     public Apple(int weight, String color, int price) {
         this.weight = weight;
         this.color = color;
@@ -23,6 +27,7 @@ public class Apple {
     }
 
     public int getPrice() {
+        System.out.println("get price called");
         return price;
     }
 
@@ -34,4 +39,14 @@ public class Apple {
                 ", price=" + price +
                 '}';
     }
+
+    public boolean isGreen() {
+        System.out.println("Is green called");
+        return color.equals("Green");
+    }
+
+    public boolean isHeavy() {
+        return weight > 200;
+    }
+
 }
